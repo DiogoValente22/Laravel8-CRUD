ad38b4ac494bec4593b8b356eeb63cb85c49624e
DiogoValente22 <diogovalentearagao@outlook.com>
2021-08-15 13:27:05 -0300

    feat: added edit route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [EmployeeController::class, 'index']);
 Route::post('/store', [EmployeeController::class, 'store'])->name('store');
 Route::get('/fetch-all', [EmployeeController::class, 'fetchAll'])->name('fetchAll');
+Route::get('/edit', [EmployeeController::class, 'edit'])->name('edit');
+
